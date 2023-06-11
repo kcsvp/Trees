51359d70e4771bc5f71131224c74505883ce51f1
kcsvp <136273856+kcsvp@users.noreply.github.com>
2023-06-11 17:09:50 -0500

    Time: 114 ms (90.06%) | Memory: 17.6 MB (19.31%) - LeetSync

---
+++ subtree-of-another-tree/subtree-of-another-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        def traverse(p,q):
+
+            if not p and not q:
+                return True
+
+            elif (not p and q) or (p and not q):
+                return False
+
+            elif p.val != q.val:
+                return False
+            
+            return traverse(p.left,q.left) and traverse(p.right,q.right)
+
+
+        def dfs(root):
+
+            if root:
+                if root.val == subRoot.val and traverse(root,subRoot):
+                    return True
+                return dfs(root.left) or dfs(root.right)
+
+            return False
+
+
+
+        return dfs(root)
