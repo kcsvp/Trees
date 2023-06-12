06ed0c9b84882b461efbb917744bb1d8961b3ce3
Karamcheti Chandra Sekhar Varun Pritham <136273856+kcsvp@users.noreply.github.com>
2023-06-12 09:50:12 -0500

    Time: 118 ms (5.47%) | Memory: 21 MB (14.56%) - LeetSync

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -9,35 +9,52 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         sol = None
 
+
         def lca(root):
             nonlocal sol
             if root:
-                
-                if root.val == p.val:
-                    if p.val < q.val:
-                        left = True
-                        right = True if lca(root.right) else False
-                    else:
-                        right = True
-                        left = True if lca(root.left) else False
-                    
-                elif root.val == q.val:
-                    if p.val < q.val:
-                        right = True
-                        left = True if lca(root.left) else False
-                    else:
-                        left = True
-                        right = True if lca(root.right) else False
-                else:
-                    left = True if lca(root.left) else False
-                    right = True if lca(root.right) else False
-
+                left = True if lca(root.left) else False
+                right = True if lca(root.right) else False
                 if left and right and not sol:
                     sol = root
+                elif (left or right) and (root == p or root == q) and not sol:
+                    sol = root 
+                elif root == p or root == q:
+                    return True
                 return left or right
+            return False
+
+        lca(root)
+
+        # def lca(root):
+        #     nonlocal sol
+        #     if root:
+                
+        #         if root.val == p.val:
+        #             if p.val < q.val:
+        #                 left = True
+        #                 right = True if lca(root.right) else False
+        #             else:
+        #                 right = True
+        #                 left = True if lca(root.left) else False
+                    
+        #         elif root.val == q.val:
+        #             if p.val < q.val:
+        #                 right = True
+        #                 left = True if lca(root.left) else False
+        #             else:
+        #                 left = True
+        #                 right = True if lca(root.right) else False
+        #         else:
+        #             left = True if lca(root.left) else False
+        #             right = True if lca(root.right) else False
+
+        #         if left and right and not sol:
+        #             sol = root
+        #         return left or right
 
                 
-            return False
+        #     return False
 
         lca(root)
         return sol
